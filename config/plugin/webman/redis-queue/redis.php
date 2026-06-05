c42fc97837038ef4291f0c86e430c505872c1411
jiechengyang <yangjiecheng1995@163.com>
2026-06-05 11:05:02 +0800

    调整queue

--- config/plugin/webman/redis-queue/redis.php
+++ config/plugin/webman/redis-queue/redis.php
@@ -4,8 +4,8 @@ return [
         'host'    => 'redis://127.0.0.1:6379',
         'options' => [
             'auth'          => null,       // 密码，字符串类型，可选参数
-            'db'            => 15,            // 数据库
-            'prefix'        => 'vr_queue',       // key 前缀
+            'db'            => 5,            // 数据库
+            'prefix'        => 'gbvr_queue',       // key 前缀
             'max_attempts'  => 5, // 消费失败后，重试次数
             'retry_seconds' => 5, // 重试间隔，单位秒
         ],
