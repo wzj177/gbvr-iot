b3859c2dda554fcf8d1331f297fecad0bdc40377
wanzij <wanzij177@163.com>
2026-07-09 14:09:49 +0800

    修复拉流bug

--- CoreW/Business/StreamProxy/Dao/Impl/StreamProxyLogDaoImpl.php
+++ CoreW/Business/StreamProxy/Dao/Impl/StreamProxyLogDaoImpl.php
@@ -22,6 +22,10 @@ class StreamProxyLogDaoImpl extends AdvancedDaoImpl implements StreamProxyLogDao
                 'id',
                 'created_at',
             ],
+            'datetime'   => [
+                'created_at',
+                'updated_at',
+            ],
             'conditions' => [
                 'id = :id',
                 'id IN (:ids)',
@@ -34,9 +38,6 @@ class StreamProxyLogDaoImpl extends AdvancedDaoImpl implements StreamProxyLogDao
                 'created_at >= :created_at_GE',
                 'created_at <= :created_at_LE',
                 'message LIKE :message_LIKE',
-            ],
-            'timestamps' => [
-                'created_at',
             ],
         ];
     }
